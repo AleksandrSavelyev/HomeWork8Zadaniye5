562d5357e0e3e03ddba22f55bf9fc5206606d681
AleksandrSavelyev <punk558721973@gmail.com>
2020-09-14 00:30:31 +0300

    soxraneniye

---
+++ HomeWork8Zadacha5/Array.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeWork8Zadacha5
+{
+    class Array
+    {
+        int n = 0;
+        int[] mas;
+
+
+        public Array(int n)
+        {
+
+            mas = new int[n];
+        }
+        public void Filing()
+        {
+            for (int i = 0; i < mas.Length; i++)
+            {
+                Console.Write($"введите элемент массива под индексом {i}:\t");
+                mas[i] = Num();
+            }
+        }
+        public void ShowMas()
+        {
+            foreach (int element in mas)
+            {
+                Console.Write(element + "\t");
+            }
+        }
+        public void SortMas()
+        {
+            int temp = 0;
+            for (int i = 0; i < mas.Length; i++)
+            {
+                for (int j = i + 1; j < mas.Length; j++)
+                {
+                    if (mas[i] < mas[j])
+                    {
+                        temp = mas[i];
+                        mas[i] = mas[j];
+                        mas[j] = temp;
+
+                    }
+                }
+            }
+            foreach (int element in mas)
+            {
+                Console.Write(element + "\t");
+            }
+        }
+        public string VolMas
+        {
+            get
+            {
+                return $"размерность массива составляет{mas.Length} ";
+            }
+        }
+        public int Skalyar
+        {
+            set
+            {
+                for (int i = 0; i < mas.Length; i++)
+                {
+                    mas[i] *= value;
+                }
+            }
+        }
+        static int Num()
+        {
+            for (; ; )
+            {
+                if (int.TryParse(Console.ReadLine(), out int a))
+                    return a;
+                else
+                    Console.Write("введите коректные данные:\t");
+            }
+        }
+    }
+}
---
+++ HomeWork8Zadacha5/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace HomeWork8Zadacha5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("введите количесто элементов массива:\t");
+            int n = num();
+            Array mas = new Array(n);
+
+            mas.Filing();
+            Console.WriteLine("элементы массива");
+            mas.ShowMas();
+            Console.WriteLine();
+            Console.WriteLine("отсортированные элементы массива");
+            mas.SortMas();
+            Console.WriteLine();
+            Console.WriteLine(mas.VolMas);
+            mas.Skalyar = 2;
+            Console.WriteLine("элементы массива умноженные на скаляр");
+            mas.ShowMas();
+
+        }
+        static int num()
+        {
+            for (; ; )
+            {
+                if (int.TryParse(Console.ReadLine(), out int a) && a > 0)
+                    return a;
+                else
+                    Console.Write("введите коректные данные:\t");
+            }
+        }
+
+    }
+}
